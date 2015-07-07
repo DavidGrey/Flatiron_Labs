de0c7497eeeaa3451c703e20a641cdb7c5752392
David Greydanus <davidgreydanus@gmail.com>
2015-07-07 10:37:52 -0400

    Create array_and_hash_challenge.rb

---
+++ Day-3/array_and_hash_challenge.rb
@@ -0,0 +1,27 @@
+favorite_movies = ["Fellowship of the Ring", 
+"The Incredibles",
+"Big Hero 6",
+"Star Trek 1",
+"Star Trek 2",
+"Star Trek 3",
+"Star Trek 4",
+"Star Trek 5",
+"Star Trek 6",
+"Star Trek 7"]
+
+
+favorite_movies.each {|movie| puts "#{movie} is one of my favorites!"}
+
+favorite_songs = {"Baby" => "Justin Bieber",
+"Roar" => "Katy Perry",
+"Party in the USA" => "Miley Cyrus",
+"Shake it Off" => "Taylor Swift",
+"Friday" => "Rebecca Black",
+"Steal My Girl" => "One Direction",
+"The Gambler" => "Kenny Rogers",
+"Country Roads"=> "John Denver",
+"Yellow Submarine" => "The Beatles",
+"For the Longest Time" => "Billy Joel"}
+
+
+favorite_songs.each {|song, artist| puts "#{song} by #{artist} is one of my favorites!"}
